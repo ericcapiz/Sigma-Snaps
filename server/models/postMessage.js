afebf9b9af64db693c2920f1cb60126f9e209e2f
ericcapiz <ericcapiz@gmail.com>
2021-03-17 14:57:32 -0500

    part 1 done

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -3,19 +3,19 @@ import mongoose from 'mongoose';
 const postSchema = mongoose.Schema({
     title: String,
     message: String,
-    creator: String,
+    creater: String,
     tags: [String],
     selectedFile: String,
-    likeCount: {
-        type: Number,
-        default: 0,
+    likeCount:{
+        type:Number,
+        default:0,
     },
-    createdAt: {
-        type: Date,
-        default: new Date(),
+    createdAt:{
+        type:Date,
+        default: new Date()
     },
-})
+});
 
-var PostMessage = mongoose.model('PostMessage', postSchema);
+const PostMessage = mongoose.model('PostMessage', postSchema);
 
 export default PostMessage;
