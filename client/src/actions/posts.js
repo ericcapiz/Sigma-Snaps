6ac41df5b7a53ad08da0339b692e08aa08ccfa08
ericcapiz <ericcapiz@gmail.com>
2021-05-16 13:14:27 -0500

    search by title or tags will display those posts to screen

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,4 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE, FETCH_BY_SEARCH } from '../constants/actionTypes';
 
 import * as api from '../api/index.js';
 
@@ -17,6 +17,7 @@ export const getPostsBySearch = (searchQuery) => async (dispatch) => {
   try {
     const {data: { data }} = await api.fetchPostsBySearch(searchQuery);
 
+    dispatch({ type: FETCH_BY_SEARCH, payload: data });
     console.log(data)
   } catch (error) {
     console.log(error)
