cbc448ba173a1f49a8ec66b1a7e28ac12fc40034
ericcapiz <ericcapiz@gmail.com>
2021-05-16 14:24:21 -0500

    updated redux for isLoading

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,15 +1,17 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE, FETCH_BY_SEARCH } from '../constants/actionTypes';
+import { START_LOADING, END_LOADING, FETCH_ALL, CREATE, UPDATE, DELETE, LIKE, FETCH_BY_SEARCH } from '../constants/actionTypes';
 
 import * as api from '../api/index.js';
 
 //ACTION CREATORS
 export const getPosts = (page) => async (dispatch) => {
   try {
+    dispatch({type: START_LOADING});
     const { data } = await api.fetchPosts(page);
 
     console.log("pagination", data)
 
     dispatch({ type: FETCH_ALL, payload: data });
+    dispatch({type: END_LOADING});
   } catch (error) {
     console.log(error.message);
   }
@@ -17,9 +19,11 @@ export const getPosts = (page) => async (dispatch) => {
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
   try {
+    dispatch({type: START_LOADING});
     const {data: { data }} = await api.fetchPostsBySearch(searchQuery);
 
     dispatch({ type: FETCH_BY_SEARCH, payload: data });
+    dispatch({type: END_LOADING});
     console.log(data)
   } catch (error) {
     console.log(error)
@@ -28,9 +32,11 @@ export const getPostsBySearch = (searchQuery) => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
   try {
+    dispatch({type: START_LOADING});
     const { data } = await api.createPost(post);
 
     dispatch({ type: CREATE, payload: data });
+    
   } catch (error) {
     console.log(error.message);
   }
